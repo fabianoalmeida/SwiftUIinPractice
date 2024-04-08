a90819f81823cb74c3583cf5c4f4350d368ef591
Fabiano Almeida <fabianoalmeida@gmail.com>
2024-04-08 15:30:53 -0300

    Initial Commit

---
+++ SwiftUIinPractice/SwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeApp.swift
+//  SwiftUIinPractice
+//
+//  Created by Fabiano Almeida on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
